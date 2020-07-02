4ea9745835268c8468d3e6b3b79fae83895a86cf
Wayne Schoeman <wschoeman@iqbusiness.net>
2020-07-02 20:10:03 +0200

    Service Created - PhoneBookModel & Controller Updated

--- PhoneBookDataAccess/PhoneBookModel.Designer.cs
+++ PhoneBookDataAccess/PhoneBookModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\IQ\ABSA_CIB_Assesment\CIBAssessment\CIBPhoneBookservice\PhoneBookDataAccess\PhoneBookModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Source\ABSACIBPhoneBook\CIBAssessment\CIBPhoneBookservice\PhoneBookDataAccess\PhoneBookModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- PhoneBookService/Controllers/PhoneBookController.cs
+++ PhoneBookService/Controllers/PhoneBookController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -7,41 +9,63 @@ using System.Web.Http;
 using PhoneBookDataAccess;
 namespace PhoneBookService.Controllers
 {
-    public class PhoneBookController : ApiController
+    public class PhoneBookController : ApiController, IDisposable
     {
         // GET: api/PhoneBook
-
-        //public IEnumerable<PhoneBook> Get()
+        //public HttpResponseMessage Get()
         //{
-        //    using (CIB_PhoneBookEntities entities = new CIB_PhoneBookEntities())
+        //    List<PhoneBook> phoneBookList = new List<PhoneBook>();
+        //    using (DbModel dc = new DbModel())
         //    {
-
+        //        phoneBookList = dc.phoneBookList.OrderBy(a => a.Name).ToList();
+        //        HttpResponseMessage response;
+        //        response = Request.CreateResponse(HttpStatusCode.OK, phoneBookList);
+        //        return response;
         //    }
         //}
-        //public IEnumerable<PhoneBook> Get()
+        public IEnumerable<PhoneBook> Get()
+        {
+            using (CIB_PhoneBookEntities entities = new CIB_PhoneBookEntities())
+            {
+                var test = entities.PhoneBooks.ToList();
+                return test;
+            }
+        }
+
+        public IEnumerable<PhoneBookEntry> GetPhoneBook(int id)
+        {
+            using (CIB_PhoneBookEntities entities = new CIB_PhoneBookEntities())
+            {
+                return entities.PhoneBookEntries.Where(e => e.phonebookid == id);
+            }
+        }
+
+        public PhoneBookEntry GetPhoneBookEntry(int id)
+        {
+            using (CIB_PhoneBookEntities entities = new CIB_PhoneBookEntities())
+            {
+                return entities.PhoneBookEntries.FirstOrDefault(e => e.phonebookentryid == id);
+            }
+        }
+        // GET: api/PhoneBook/5
+        //public string Get(int id)
         //{
-        //    return new string[] { "value1", "value2" };
+        //    return "value";
         //}
 
-        // GET: api/PhoneBook/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
         // POST: api/PhoneBook
-        public void Post([FromBody]string value)
-        {
-        }
+        //public void Post([FromBody]string value)
+        //{
+        //}
 
-        // PUT: api/PhoneBook/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //// PUT: api/PhoneBook/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
 
-        // DELETE: api/PhoneBook/5
-        public void Delete(int id)
-        {
-        }
+        //// DELETE: api/PhoneBook/5
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
