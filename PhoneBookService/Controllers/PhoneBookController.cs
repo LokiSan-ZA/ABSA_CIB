f6e438ee559a432abf2b1c4350c0f8b843be5bec
Wayne Schoeman <wschoeman@iqbusiness.net>
2020-07-02 16:43:15 +0200

    Add project files.

---
+++ PhoneBookService/Controllers/PhoneBookController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PhoneBookDataAccess;
+namespace PhoneBookService.Controllers
+{
+    public class PhoneBookController : ApiController
+    {
+        // GET: api/PhoneBook
+
+        //public IEnumerable<PhoneBook> Get()
+        //{
+        //    using (CIB_PhoneBookEntities entities = new CIB_PhoneBookEntities())
+        //    {
+
+        //    }
+        //}
+        //public IEnumerable<PhoneBook> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET: api/PhoneBook/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/PhoneBook
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/PhoneBook/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/PhoneBook/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
